544df9c461c8e00b136e79cbab35bc076adb2638
josephpbermingham <josephpbermingham@gmail.com>
2018-10-25 08:50:20 -0400

    added comments to explain task

--- cpu.c
+++ cpu.c
@@ -1,6 +1,31 @@
-void set_reg(int reg, int value);
-int get_reg(int reg);
-int get_cpsr();
-void show_regs();
-void step();
-void step_n(int n);
+int reg[16];
+int cpsr;//status register
+
+//sets the value of reg to value
+void set_reg(int reg, int value){
+reg[reg]=value;
+}
+//returns the value of the register number (int reg)
+int get_reg(int reg){
+	
+	}
+	//returns the contents of cpsr
+int get_cpsr(){
+	
+	}
+	//prints out the contents of reg
+void show_regs(){
+	
+}
+/*
+ * does one iteration of fetch-exicute-decode cycle
+ * fetches instructions pointed to by reg
+ * decodes them (int opcode = inst>>24;)
+ * execute (opcode switch statement)
+*/
+void step(){
+	
+}
+void step_n(int n){
+	
+}
